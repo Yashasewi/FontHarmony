d24afd27c50919e32e08fd7c437f383738135217
Yashasewi <Yashasewisingh2668@gmail.com>
2023-07-02 19:19:20 +0530

    new project

---
+++ src/components/herooo/Hero.tsx
@@ -0,0 +1,10 @@
+import "./Hero.css";
+function Hero() {
+  return (
+    <div className="min-h-screen background flex justify-center  items-center">
+      <h1 className=" text-[16rem]  ">FONTHARMONY</h1>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,10 @@
+export default function Nav() {
+  return (
+    <div className=" absolute top-[15%] font-Instrument flex justify-between min-w-full pr-20 pl-20">
+      <h5 className=" uppercase font-light tracking-tight ">
+        Discover Font Harmony
+      </h5>
+      <h5 className="uppercase  font-light tracking-tight "> 2023</h5>
+    </div>
+  );
+}
