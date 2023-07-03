bee20dbe8efec62bad6a76c8fb5db352e02b94df
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-07-03 12:26:24 +0530

    added animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,35 @@
 import Nav from "./nav/Nav";
-
+import { motion } from "framer-motion";
 function Hero() {
+  const variants = {
+    hidden: { opacity: 0, y: -100 },
+    visible: { opacity: 1, y: 0 },
+  };
+  const variants2 = {
+    hidden: { opacity: 0, y: 100 },
+    visible: { opacity: 1, y: 0 },
+  };
   return (
-    <div>
-      <div className="min-h-screen min-w-full background flex gap-y-8 flex-col justify-center  items-center">
+    <motion.div>
+      <motion.div
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5 }}
+        className="min-h-screen min-w-full background flex gap-y-8 flex-col justify-center  items-center"
+      >
         <Nav />
-        <h1 className="text-[16rem]">FONTHARMONY</h1>
-      </div>
-    </div>
+        <motion.h1
+          variants={variants2}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 0.8, delay: 0.5 }}
+          className="text-[16rem]"
+        >
+          FONTHARMONY
+        </motion.h1>
+      </motion.div>
+    </motion.div>
   );
 }
 
