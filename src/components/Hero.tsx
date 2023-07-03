17fa67924850d6fa7f0dfa2ff8439e265bd945ac
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-07-03 14:52:46 +0530

    added discription

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,6 +28,83 @@ function Hero() {
         >
           FONTHARMONY
         </motion.h1>
+        <motion.div
+          style={{ fontFamily: "'Roboto Condensed', sans-serif" }}
+          className=" text-xs py-10"
+        >
+          <div className="tracking-tighter">
+            <p className="text-center">
+              All fonts used are available for free on{" "}
+              <a
+                className="text-fuchsia-800 hover:text-fuchsia-600"
+                href="https://www.google.com/fonts"
+                target="_blank"
+              >
+                Google Fonts
+              </a>{" "}
+              or elsewhere on the interwebs. Ipsums found via{" "}
+              <a
+                className="text-fuchsia-800 hover:text-fuchsia-600"
+                href="http://meettheipsums.com/"
+                target="_blank"
+              >
+                meettheipsums.com
+              </a>
+              . This site was inspired by{" "}
+              <a
+                className="text-fuchsia-800 hover:text-fuchsia-600"
+                href="http://hellohappy.org/beautiful-web-type/"
+                target="_blank"
+              >
+                this
+              </a>
+              ,{" "}
+              <a
+                className="text-fuchsia-800 hover:text-fuchsia-600"
+                href="http://bueltge.de/free-web-font-combinations/"
+                target="_blank"
+              >
+                this,{" "}
+              </a>
+              <a
+                className="text-fuchsia-800 hover:text-fuchsia-600"
+                href="http://bchiang7.github.io/fontipsums/"
+                target="_blank"
+              >
+                this
+              </a>
+              , and{" "}
+              <a
+                className="text-fuchsia-800 hover:text-fuchsia-600"
+                href="http://femmebot.github.io/google-type/"
+                target="_blank"
+              >
+                this
+              </a>
+              . Click the text examples to get the typeface!
+            </p>
+          </div>
+          <div className="text-center p-10 py-0 text-xs">
+            <p>
+              Maintained by{" "}
+              <a
+                className="px-1 text-fuchsia-800 hover:text-fuchsia-600"
+                href="https/github.com/yashasewi"
+                target="_blank"
+              >
+                Yashasewi Singh
+              </a>
+              Fork this repo on{" "}
+              <a
+                className="py-1 text-fuchsia-800 hover:text-fuchsia-600"
+                href="https/github.com/yashasewi"
+                target="_blank"
+              >
+                Github
+              </a>
+            </p>
+          </div>
+        </motion.div>
       </motion.div>
     </motion.div>
   );
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,6 +1,9 @@
 export default function Nav() {
   return (
-    <div className=" font-Instrument flex justify-between min-w-full pr-32 pl-32">
+    <div
+      style={{ fontFamily: "'Roboto Condensed', sans-serif" }}
+      className="  flex justify-between min-w-full pr-32 pl-32"
+    >
       <h5 className=" uppercase font-light tracking-tight ">
         Discover Font Harmony
       </h5>
