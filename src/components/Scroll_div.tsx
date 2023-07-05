819aa03770822b1d3697d3702bcf1a141aca3451
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-07-05 11:13:57 +0530

    added the loader

--- src/components/Scroll_div.tsx
+++ src/components/Scroll_div.tsx
@@ -1,53 +1,59 @@
 import { colors, fonts_Data } from "./util";
-import { motion, useAnimate, useInView } from "framer-motion";
-import { useEffect, useRef } from "react";
-
+import { motion, useScroll, useSpring } from "framer-motion";
+import { useRef } from "react";
 export default function Scroll_div() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   const ref = useRef(null);
-  const isInView = useInView(ref);
-
-  useEffect(() => {
-    console.log(isInView);
-  }, [isInView]);
-
   return (
-    <div className="scroll_div  flex flex-col ">
-      {colors.map((color, index) => (
-        <div
-          className="h-[100vh] container "
-          key={index}
-          style={{ backgroundColor: color }}
-        >
+    <div>
+      <motion.div
+        className="progress-bar"
+        style={{ scaleX, backgroundColor: "GhostWhite" }}
+      />
+
+      <div className="scroll_div  flex flex-col ">
+        {colors.map((color, index) => (
           <div
-            className="font-pair"
-            style={{ fontFamily: "'Roboto Condensed', sans-serif" }}
+            className="h-[100vh] container "
+            key={index}
+            style={{ backgroundColor: color }}
           >
-            <p>
-              <a href={fonts_Data[index]["font-1"].link}>
-                {fonts_Data[index]["font-1"].name}
-              </a>{" "}
-              ,{" "}
-              <a href={fonts_Data[index]["font-2"].link}>
-                {fonts_Data[index]["font-2"].name}
-              </a>
-            </p>
-          </div>
-          <div className="text-wrapper">
-            <h1
-              className="uppercase"
-              style={{ fontFamily: fonts_Data[index]["font-1"].name }}
+            <div
+              className="font-pair"
+              style={{ fontFamily: "'Roboto Condensed', sans-serif" }}
             >
-              {fonts_Data[index].Heading}
-            </h1>
-            <p
-              className=" "
-              style={{ fontFamily: fonts_Data[index]["font-2"].name }}
-            >
-              {fonts_Data[index].Body}
-            </p>
+              <p>
+                <a href={fonts_Data[index]["font-1"].link}>
+                  {fonts_Data[index]["font-1"].name}
+                </a>{" "}
+                ,{" "}
+                <a href={fonts_Data[index]["font-2"].link}>
+                  {fonts_Data[index]["font-2"].name}
+                </a>
+              </p>
+            </div>
+            <div className="text-wrapper">
+              <h1
+                className="uppercase"
+                style={{ fontFamily: fonts_Data[index]["font-1"].name }}
+              >
+                {fonts_Data[index].Heading}
+              </h1>
+              <p
+                className=" "
+                style={{ fontFamily: fonts_Data[index]["font-2"].name }}
+              >
+                {fonts_Data[index].Body}
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
